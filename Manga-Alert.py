f22663b2352e0d4e69e16e8f00296a5a16d9b798
Shintany <irchadtuankitchil@gmail.com>
2018-05-31 23:00:28 +0200

    Created a second test - internetAccess

--- Manga-Alert.py
+++ Manga-Alert.py
@@ -3,6 +3,6 @@ from test import Test
 
 if __name__ == "__main__":
     filename = "list.csv"
-    t = Test(filename)
-    t.run()
+    test = Test(filename)
+    test.run()
 
--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import os.path
+import socket
 
 class Test():
     def __init__(self, filePath):
-        print("---Tests---")
+        print("\t---Tests---")
         self.path = filePath
 
     def run(self):
         try:
             self.fileExist()
+            self.internetAccess()
         except Exception as error:
             print(repr(error))
-
 
     def fileExist(self):
         if os.path.exists(self.path):
@@ -18,3 +19,15 @@ class Test():
         else:
             print("File exist : Error")
             raise Exception('File was not found')
+    
+    def internetAccess(self):
+        str_google = "www.google.com"
+        try:
+            socket.gethostbyname(str_google)
+            print("Internet access : OK")
+        except OSError as error:
+            print("Internet access : Error")
+            print("No internet access...")
+            raise Exception(error)
+
+        
