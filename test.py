18969f0bdccbe6849c54a3001f8972857dddd239
Shintany <irchadtuankitchil@gmail.com>
2018-05-31 23:14:39 +0200

    Third test created - Japscan website Access

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-import os.path
-import socket
 
 class Test():
     def __init__(self, filePath):
@@ -8,19 +6,22 @@ class Test():
 
     def run(self):
         try:
-            self.fileExist()
-            self.internetAccess()
+            self.doesFileExist()
+            self.haveInternetAccess()
+            self.isWebSiteUp()
         except Exception as error:
             print(repr(error))
 
-    def fileExist(self):
+    def doesFileExist(self):
+        import os.path
         if os.path.exists(self.path):
             print("File exist : OK")
         else:
             print("File exist : Error")
             raise Exception('File was not found')
     
-    def internetAccess(self):
+    def haveInternetAccess(self):
+        import socket
         str_google = "www.google.com"
         try:
             socket.gethostbyname(str_google)
@@ -29,5 +30,17 @@ class Test():
             print("Internet access : Error")
             print("No internet access...")
             raise Exception(error)
+
+    def isWebSiteUp(self):
+        import requests
+        #Web site where I want to check latest release
+        website_url = "http://www.japscan.cc"
+        response = requests.get(website_url, stream=True)
+        if response.status_code != 200:
+            print("Japscan website access : Error")
+            raise Exception("Japscan must be down...")
+        else:
+            print("Japscan website access : OK")
+
 
         
