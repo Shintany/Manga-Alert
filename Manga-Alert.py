8b999095480ab29b4bef9afd05f0f2ee56f4beda
Shintany <irchadtuankitchil@gmail.com>
2018-06-01 22:57:32 +0200

    Created a Database class that will put in a dictionnary the manga linked to the chapter number

--- Manga-Alert.py
+++ Manga-Alert.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 from test import Test
+from tools import Database
+import sys
 
 filename = "list.csv"
 test = Test(filename)
@@ -8,6 +10,19 @@ test.run()
 
 if __name__ == "__main__":
     print("Tests : OK")
+
+    if len(sys.argv) != 2:
+        print ("Argument missing...")
+        print("Usage : python3 Manga-Alert.py $databasePath")
+        exit()
+    databasePath = sys.argv[1]
+    print(databasePath)
+
+    # Filling the dictionnary with the manga in the .csv file
+    db = Database(databasePath)
+    print(db.database.get("One piece"))
+    print(db.database.get("My Hero Academia"))
+
     website_url = "http://www.japscan.cc/mangas/"
     list_path = "list.csv"
 
@@ -19,9 +34,11 @@ if __name__ == "__main__":
     # Search for <div id="liste_mangas">
 
     content = soup.find('div', {'id' : 'liste_mangas'})
+    # If he doesn't find it -> quit the program
     if type(content) == 'NoneType':
-        print("Not found")
+        print("<div id=\"liste_mangas\"> was not found...")
+        exit()
     else:
         print("Successfully Found")
+        # for link in content.find_all('a', 'href' ):
 
-    
---
+++ tools.py
@@ -0,0 +1,15 @@
+import csv
+
+# This class purpose is to put into an array the list of manga from the .csv file
+class Database():
+    def __init__(self, filePath):
+        self.database = {}
+        self.databasePath = filePath
+
+        self.fillDatabase()
+    
+    def fillDatabase(self):
+        with open(self.databasePath, mode = 'r') as infile:
+            dbReader = csv.reader(infile)
+            self.database = {row[0]:row[1] for row in dbReader}
+    
