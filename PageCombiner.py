940df0683bf18685c979da2f69b77452033bfdbf
Shintany <irchadtuankitchil@gmail.com>
2018-06-21 21:02:16 +0200

    Now downloading chapter's first page, now have to download every page of it

--- PageCombiner.py
+++ PageCombiner.py
@@ -1,16 +1,18 @@
 from PIL import Image
 import requests
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from urllib.request import Request
 
 class PageCombiner():
     def __init__(self, _name, _chapter, _url):
-        print("Combining chapter's image...")
 
         # Attributes
         self.name = _name
         self.chapter = _chapter
         self.url = _url
         self.response = requests.get(self.url, stream = True)
+        self.data = ""
 
         # Check if we got a response from the url
         if(self.response.status_code == 200):
@@ -24,12 +26,11 @@ class PageCombiner():
             self.run()
 
     def countPage(self):
-        print("Counting pages..")
 
         # List all the <a> tags with attribute class = "pagi"
         content = self.soup.find_all('a', {'class' : 'pagi'})
         for pageNb in content:
-            
+
             # Get <a> tag content until the last one
             lastPage = int(pageNb.get_text())
         return lastPage
@@ -39,13 +40,22 @@ class PageCombiner():
         
         if( self.response.status_code == 200 ):
 
-            print("The chapter " + str(self.chapter) + " contain " + str(self.pageCount) + " pages")
-
             soup = BeautifulSoup(self.data, 'lxml')
 
-            # Find how many pages there are
+            # Initialize counter
+            i = 1
+            while i <= self.pageCount:
 
-            # Find src attribute of <img> tag
+                # Find id attribute of <img> tag with "image" as value
+                img_soup = soup.find('img', {'id' : 'image'})
+                print(img_soup['src'])
+
+                # Download image
+                user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46'
+                img = open(img_soup['data-img'], 'wb')
+                img.write( urlopen( Request(img_soup['src'], headers={'User-Agent': user_agent})).read() )
+                img.close()
+                i = i + 1
 
         else:
             print("The url send an error code : " + str(self.response.status_code))
