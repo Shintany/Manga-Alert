c98b0d06443fdab381f6903d7a0eb7d0202a1436
Shintany <irchadtuankitchil@gmail.com>
2018-06-25 16:44:57 +0200

    Created an .sh script to install necessary packages

---
+++ install/install.sh
@@ -0,0 +1,23 @@
+
+# Update basic package
+sudo apt-get -y update
+
+# Install pip3 package installer
+sudo apt-get -y install python3-pip
+
+# Install Requests
+sudo pip3 install requests
+
+# Install lxml parser
+sudo pip3 install lxml
+
+# Install BeautifulSoup4
+sudo pip3 install bs4
+
+# Uninstall then reinstall Pillow
+
+# Install PIL missing package
+sudo apt-get -y install libopenjp2-7
+
+# Install TIFF library
+sudo apt-get -y install libtiff5
