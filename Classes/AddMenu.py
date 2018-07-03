7cef8ca3eb1508ee60aed25c90a8860ab05cdba8
Shintany <irchadtuankitchil@gmail.com>
2018-07-03 19:46:26 +0200

    The user can now add manga by launching the program with add as parameter

--- Classes/AddMenu.py
+++ Classes/AddMenu.py
@@ -9,12 +9,22 @@ class Menu():
         # Get Manga List
         self.manga_list = self.getMangaName()
 
-        print('There\'s ' + str(len(self.manga_list)) + ' mangas')
+        continue_to_add = True
+
+        while continue_to_add:
+            # Get the Manga name fron the user
+            self.addManga()
+
+            # Ask whether the user want to add another manga
+            choice = input('Add another manga? (Y/n) : ')
+            if choice == 'N' or choice == 'n':
+                continue_to_add = False
+
 
     def getMangaName(self):
 
-        print('Getting manga list')
-
+        # Initializing void dictionnary
+        manga_list = []
         manga_list_response = requests.get(self.url)
         
         # Get the data from the url in text format
@@ -29,15 +39,32 @@ class Menu():
             print("<div id=\"liste_mangas\"> was not found...")
             exit()
 
-        manga_list = []
-
-        print('Going into the loop')
+        # Getting all manga name and put them in a dictionnary
         for manga in manga_list_content.find_all('a'):
             if '/mangas/' in manga['href']:
                 manga_name = manga['href'].replace('/mangas/', '')
                 manga_name = manga_name.replace('/', '')
-                # print('Manga : ' + manga_name)
                 manga_list.append(manga_name)
         
         return manga_list
+
+    def addManga(self):
+
+        # Getting manga name and chapter
+        manga_name = input('Enter the manga name : ')
+        last_chapter_read = int(input('The last chapter number you\'ve read : '))
+
+        # Creating string to add in the .csv file
+        adding_line = manga_name + ',' + str(last_chapter_read) + '\n'
+
+        try:
+            # Append the line into .csv file
+            with open('list.csv', 'a') as db:
+                db.write(adding_line)
+        except:
+            print('Error when writing in the .csv file')
+            exit()
+        
+        print('Line added succesfully!')
+
 
